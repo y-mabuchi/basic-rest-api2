a01f52829031b08457ed0c9bc4c0970a248655b4
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-11-24 09:54:41 +0900

    initial commit

---
+++ app/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const sqlit3 = require('sqlite3');
+const dbPath = 'app/db/database.sqlite3';
+
+// usersを全件取得
+app.get('/api/v1/users', (req, res) => {
+  // DBに接続
+  const db = new sqlit3.Database(dbPath);
+
+  // 全件取得なのでallを使う
+  db.all('SELECT * FROM users', (err, rows) => {
+    // エラー処理については書かない
+    // まずは正常系だけ
+    res.json(rows);
+  });
+
+  // DBをクローズ
+  db.close();
+});
+
+// 単一のuserのAPI
+app.get('/api/v1/users/:id', (req, res) => {
+  // DBに接続
+  const db = new sqlit3.Database(dbPath);
+
+  // idを取り出す
+  const id = req.params.id;
+
+  // 単一なのでgetを使う
+  // バッククォーテーションでテンプレートリテラルを使う
+  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+    // エラー処理については書かない
+    // まずは正常系だけ
+    res.json(row);
+  });
+
+  // DBをクローズ
+  db.close();
+});
+
+// 検索のAPI
+// ex) /api/v1/users/serach?q=keyword
+app.get('/api/v1/search', (req, res) => {
+  // DBに接続
+  const db = new sqlit3.Database(dbPath);
+
+  // クエリの中身を取得する
+  const keyword = req.query.q;
+
+  // 複数取得するのでallを使う
+  // バッククォーテーションでテンプレートリテラルを使う
+  db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+    // エラー処理については書かない
+    // まずは正常系だけ
+    res.json(rows);
+  });
+
+  // DBをクローズ
+  db.close();
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listen on port: ' + port);
